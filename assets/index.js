01db861322bce64c40f79a8aad8b1a965c54a911
dreae <penitenttangentt@gmail.com>
2016-02-17 21:46:20 -0500

    Import jquery, fix warning in materialize

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
+import "../node_modules/jquery/dist/jquery.js";
 import "../node_modules/materialize-css/dist/css/materialize.css";
-import "../node_modules/materialize-css/dist/js/materialize.js";
+import "imports?require=>undefined!../node_modules/materialize-css/dist/js/materialize.js";
 import "./styles/style.css";
 import "./index.html";
 
