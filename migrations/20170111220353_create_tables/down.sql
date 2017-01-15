55e327b79ba6a8c8e73f04225ac1af619fabbbeb
Dreae <dreae@dreae.onl>
2017-01-14 20:16:39 -0500

    Create database tables

--- migrations/20170111220353_create_tables/down.sql
+++ migrations/20170111220353_create_tables/down.sql
@@ -1,1 +1,7 @@
+DROP TABLE slaves;
+DROP TYPE OS;
+DROP TABLE user_servers;
+DROP TABLE servers;
+DROP TABLE games;
+DROP TABLE plugins;
 DROP TABLE users;
--- migrations/20170111220353_create_tables/up.sql
+++ migrations/20170111220353_create_tables/up.sql
@@ -3,3 +3,37 @@ CREATE TABLE users (
   username VARCHAR NOT NULL,
   password VARCHAR NOT NULL
 );
+
+CREATE TABLE plugins (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  linux_so VARCHAR NOT NULL,
+  windows_dll VARCHAR NOT NULL
+);
+
+CREATE TABLE games (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  plugin INTEGER REFERENCES plugins(id)
+);
+
+CREATE TABLE servers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  game INTEGER REFERENCES games(id)
+);
+
+CREATE TABLE user_servers (
+  user_id INTEGER REFERENCES users(id),
+  server_id INTEGER REFERENCES servers(id),
+  PRIMARY KEY (user_id, server_id)
+);
+
+CREATE TYPE OS AS ENUM ('windows', 'linux');
+
+CREATE TABLE slaves (
+  id SERIAL PRIMARY KEY,
+  ip VARCHAR NOT NULL,
+  port INTEGER NOT NULL DEFAULT 26015,
+  os OS
+);
